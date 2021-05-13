236c755355893816a069279cad1bab571348cba1
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-05-14 02:08:56 +0700

    add Absensi model

---
+++ app/Models/Absensi.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Absensi extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'kelas_id',
+        'status'
+    ];
+
+    public function kelas()
+    {
+        return $this->belongsTo(Kelas::class);
+    }
+}
