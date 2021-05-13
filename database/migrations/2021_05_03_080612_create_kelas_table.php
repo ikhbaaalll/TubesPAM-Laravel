4b8e535e0a0651e35850fe3ad9937be440c174d1
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-05-14 02:09:14 +0700

    add table kelas

---
+++ database/migrations/2021_05_03_080612_create_kelas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKelasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kelas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('nama');
+            $table->string('pelajaran');
+            $table->string('qr_code')->nullable();
+            $table->date('tanggal');
+            $table->time('waktu');
+            $table->integer('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kelas');
+    }
+}
