136db57da8c3a645e8066d73b2fea493e2f553b0
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-05-15 00:23:51 +0700

    update model Absensi

--- app/Models/Absensi.php
+++ app/Models/Absensi.php
@@ -19,4 +19,9 @@ class Absensi extends Model
     {
         return $this->belongsTo(Kelas::class);
     }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
